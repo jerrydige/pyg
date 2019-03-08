0430d516ca9338132f1ba200894474cdbe029ae6
jerrychan <jerrydige@163.com>
2019-03-08 21:08:29 +0800

    品优购

---
+++ dist/js/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$(function () {
+  var gallery = mui('.mui-slider');
+  gallery.slider({
+    interval: 5000 //自动轮播周期，若为0则不自动播放，默认为0；
+
+  });
+});
+//# sourceMappingURL=index.js.map
