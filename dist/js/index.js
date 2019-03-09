3deaf807b3fae4d203b1dcdb51c902c31c4567d6
jerrychan <jerrydige@163.com>
2019-03-09 21:40:03 +0800

    完成一半

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,10 +1,54 @@
 "use strict";
 
 $(function () {
-  var gallery = mui('.mui-slider');
-  gallery.slider({
-    interval: 5000 //自动轮播周期，若为0则不自动播放，默认为0；
+  init();
 
-  });
+  function init() {
+    swiperdata();
+    catitems();
+  } // 轮播图
+
+
+  function swiperdata() {
+    $.ajax({
+      url: "http://api.pyg.ak48.xyz/api/public/v1/home/swiperdata",
+      type: "get",
+      success: function success(result) {
+        if (result.meta.status === 200) {
+          var data = result.data;
+          var html = template("swiperTpl", {
+            arr: data
+          });
+          $(".pyg_slide").html(html);
+          var gallery = mui('.mui-slider');
+          gallery.slider({
+            interval: 5000 //自动轮播周期，若为0则不自动播放，默认为0；
+
+          });
+        } else {
+          console.log("失败", result);
+        }
+      }
+    });
+  } // 分类
+
+
+  function catitems() {
+    $.get("http://api.pyg.ak48.xyz/api/public/v1/home/catitems", function (result) {
+      if (result.meta.status === 200) {
+        var data = result.data;
+        var html = "";
+
+        for (var i = 0; i < data.length; i++) {
+          var tmpHtml = "\n        <a href=\"javascript:;\">\n        <img src=\"".concat(data[i].image_src, "\" alt=\"\">\n        </a>\n        ");
+          html += tmpHtml;
+        }
+
+        $(".pyg_cates").html(html);
+      } else {
+        console.log("失败", result);
+      }
+    });
+  }
 });
 //# sourceMappingURL=index.js.map
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,54 @@
 $(function () {
-  var gallery = mui('.mui-slider');
-gallery.slider({
-  interval:5000//自动轮播周期，若为0则不自动播放，默认为0；
-});
-  
+  init();
+
+  function init() {
+    swiperdata();
+    catitems();
+  }
+
+  // 轮播图
+  function swiperdata() {
+    $.ajax({
+      url: "http://api.pyg.ak48.xyz/api/public/v1/home/swiperdata",
+      type: "get",
+      success: function (result) {
+        if (result.meta.status === 200) {
+          let data = result.data;
+          let html = template("swiperTpl", {
+            arr: data
+          });
+          $(".pyg_slide").html(html);
+
+          var gallery = mui('.mui-slider');
+          gallery.slider({
+            interval: 5000 //自动轮播周期，若为0则不自动播放，默认为0；
+          });
+        } else {
+          console.log("失败", result);
+        }
+      }
+    })
+  }
+
+  // 分类
+  function catitems() {
+    $.get("http://api.pyg.ak48.xyz/api/public/v1/home/catitems", result => {
+      if (result.meta.status === 200) {
+        let data = result.data;
+        let html = "";
+        for (let i = 0; i < data.length; i++) {
+          let tmpHtml = `
+        <a href="javascript:;">
+        <img src="${data[i].image_src}" alt="">
+        </a>
+        `;
+          html += tmpHtml;
+        }
+        $(".pyg_cates").html(html);
+
+      } else {
+        console.log("失败", result);
+      }
+    });
+  }
 })
